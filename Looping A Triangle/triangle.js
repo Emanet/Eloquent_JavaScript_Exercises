79ed27efe7a828b50f51f3fd28a218b40b57563f
Emanet <mehmetcanemanet@gmail.com>
2021-06-12 12:59:52 +0300

    Looping Triangle

---
+++ Looping A Triangle/triangle.js
@@ -0,0 +1,10 @@
+/* for(let triangle = '#'; triangle.length <= 7; triangle += '#'){
+    console.log(triangle);
+}
+ */
+
+let triangle = '';
+while (triangle.length <= 7) {
+    triangle += '#';
+    console.log(triangle);
+}
