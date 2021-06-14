a159dcc66be3eb7e6577fd364ea69e6f13caa985
Emanet <mehmetcanemanet@gmail.com>
2021-06-14 23:47:20 +0300

    Functions

---
+++ Ch3_Functions/defining_func.js
@@ -0,0 +1,31 @@
+/* function square(x) {
+    return x * x;
+}
+console.log(square(12));
+ */
+
+/* const power = function (base,exponent) {
+    let result = 1;
+    for(let count = 0; count < exponent; count++){
+        result *= base;
+    }
+    return result;
+};
+console.log(power(2,10)); */
+
+
+
+//Call Stack
+/* function greet(who) {
+    console.log("Hello" + who);
+}
+greet("Harry");
+console.log("Bye"); */
+//----------
+function chicken() {
+    return egg();
+};
+function egg() {
+    return chicken();
+}
+console.log(chicken()+"came firs.");
